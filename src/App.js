cc65214a2e4ae3d9e5f28c4a1a90ca7b0070a41b
Oksana Karpova <oksanachv@gmail.com>
2022-12-16 21:04:57 +0100

    Added component Content to project

--- src/App.js
+++ src/App.js
@@ -1,6 +1,7 @@
 import { Container } from "@mui/material";
 import { ThemeProvider } from "@emotion/react";
 import Banner from "./components/Banner";
+import Content from "./components/Content";
 import Colors from "./styles/index";
 import theme from "./styles/index";
 function App() {
@@ -8,6 +9,7 @@ function App() {
     <ThemeProvider theme={theme}>
       <Container maxWidth="xl" sx={{ background: Colors.white }}>
         <Banner />
+        <Content />
       </Container>
     </ThemeProvider>
   );
---
+++ src/components/Content/index.js
@@ -0,0 +1,7 @@
+import * as React from "react";
+
+const Content = () => {
+  return <h1>Products</h1>;
+};
+
+export default Content;
