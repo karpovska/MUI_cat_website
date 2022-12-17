1fdf00b285ffbdc15153abb40f9c499e17383c1c
Oksana Karpova <oksanachv@gmail.com>
2022-12-17 12:21:00 +0100

    Added space between header and banner

--- src/App.js
+++ src/App.js
@@ -1,18 +1,18 @@
-import { Container } from "@mui/material";
+import { Stack } from "@mui/material";
 import { ThemeProvider } from "@emotion/react";
 import Banner from "./components/Banner";
-import Colors from "./styles/index";
+//import Colors from "./styles/index";
 import theme from "./styles/index";
 import ActionIcons from "./components/ActionIcons/ActionIcons";
 import NavBar from "./components/Header/Header";
 function App() {
   return (
     <ThemeProvider theme={theme}>
-      <Container maxWidth="xl" sx={{ background: Colors.white }}>
+      <Stack spacing={10}>
         <NavBar />
         <ActionIcons />
         <Banner />
-      </Container>
+      </Stack>
     </ThemeProvider>
   );
 }
