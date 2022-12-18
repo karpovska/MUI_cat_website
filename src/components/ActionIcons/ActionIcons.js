e667d2c4f8d92d9d5450683854c710c97a6decf7
TryNoRage <shaiturovtr@gmail.com>
2022-12-18 10:32:23 +0200

    Fixed bugs: menu, header

--- src/components/ActionIcons/ActionIcons.js
+++ src/components/ActionIcons/ActionIcons.js
@@ -5,26 +5,25 @@ import FavoriteIcon from '@mui/icons-material/Favorite';
 import NotificationsIcon from '@mui/icons-material/Notifications';
 import PersonIcon from '@mui/icons-material/Person';
 import { Colors } from "../../styles/index";
-import { Divider, useMediaQuery} from '@mui/material';
+import { Divider, Paper, useMediaQuery} from '@mui/material';
 
 
 export default function ActionIcons() {
   const showForLargeScreen  = useMediaQuery('(max-width:900px)');
   return (
-    <>
-      {showForLargeScreen 
-      && 
-      <Box sx={{ width:"100%", position:"fixed", bottom:0}} >
+    <>{showForLargeScreen &&
+    <Paper sx={{ position: 'fixed', bottom: 0, left: 0, right: 0, zIndex:1300}}>
         <BottomNavigation sx={{"&.MuiBottomNavigation-root": {bgcolor: Colors.shaft}, height:40}}>
-          <BottomNavigationAction icon={<FavoriteIcon />}  sx={{color: Colors.primary}}/>
-          <Divider  orientation="vertical" flexItem sx={{bgcolor:Colors.primary}}/>
-          <BottomNavigationAction  icon={<NotificationsIcon />} sx={{color: Colors.primary}}/>
-          <Divider  orientation="vertical" flexItem sx={{bgcolor:Colors.primary}}/>
-          <BottomNavigationAction  icon={<PersonIcon />} sx={{color: Colors.primary}} />
-      </BottomNavigation>     
-    </Box>
-      }
+          <BottomNavigationAction label="Favourite" icon={<FavoriteIcon sx={{color: Colors.primary}}/>} sx={{borderRight:"1px solid #00c7c0"}} />
+          <BottomNavigationAction label="Notifications" icon={<NotificationsIcon sx={{color: Colors.primary}} />} />
+          <BottomNavigationAction label="Account" icon={<PersonIcon sx={{color: Colors.primary}}/>} sx={{borderLeft:"1px solid #00c7c0"}} />
+        </BottomNavigation>
+      </Paper>
+    }
+      
+
     </>
     
   );
 }
+
--- src/components/Header/Header.js
+++ src/components/Header/Header.js
@@ -8,8 +8,8 @@ function NavBar() {
   return (
     <AppBar sx={{ background: "#fff" }}>
       <Toolbar>
-        <BackMenu />
-        <Typography variant="h3" sx={{ flexGrow: 1, color: Colors.primary }}>
+        <BackMenu/>
+        <Typography variant="h3" sx={{ flexGrow: 1, color: Colors.primary, textAlign:"center" }}>
           Cats&Friends
         </Typography>
         <NavMenu />
